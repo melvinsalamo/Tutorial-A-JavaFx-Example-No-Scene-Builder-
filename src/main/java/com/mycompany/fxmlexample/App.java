3fba55b474642af4166bfbbbb950cd9c51235d7d
meL <meL@meL>
2022-11-03 21:59:50 -0400

    Running a JavaFx Program from Oracle Tutorial; Tutorial-A with no Scene Builder

---
+++ src/main/java/com/mycompany/fxmlexample/App.java
@@ -0,0 +1,29 @@
+package com.mycompany.fxmlexample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+     @Override
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
